39eea2f1bc12a808ec82ffdeb5651d3ff9640b34
trandai130804 <quocdai138@gmail.com>
2024-11-28 21:30:04 +0700

    Baitap28.11

---
+++ BaiTap2(28.11)/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BaiTap2_28._11_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.txtSlot1 = new System.Windows.Forms.TextBox();
+            this.txtSlot2 = new System.Windows.Forms.TextBox();
+            this.txtSlot3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lv1
+            // 
+            this.lv1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.LastName,
+            this.FirstName,
+            this.Phone});
+            this.lv1.FullRowSelect = true;
+            this.lv1.GridLines = true;
+            this.lv1.HideSelection = false;
+            this.lv1.Location = new System.Drawing.Point(63, 62);
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(469, 299);
+            this.lv1.TabIndex = 0;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            this.lv1.View = System.Windows.Forms.View.Details;
+            this.lv1.Click += new System.EventHandler(this.lv1_Click);
+            // 
+            // LastName
+            // 
+            this.LastName.Text = "LastName";
+            // 
+            // FirstName
+            // 
+            this.FirstName.Text = "FirstName";
+            // 
+            // Phone
+            // 
+            this.Phone.Text = "Phone";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(178, 393);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(102, 26);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(341, 392);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(91, 24);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(488, 393);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(88, 23);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // txtSlot1
+            // 
+            this.txtSlot1.Location = new System.Drawing.Point(609, 117);
+            this.txtSlot1.Name = "txtSlot1";
+            this.txtSlot1.Size = new System.Drawing.Size(160, 22);
+            this.txtSlot1.TabIndex = 4;
+            // 
+            // txtSlot2
+            // 
+            this.txtSlot2.Location = new System.Drawing.Point(609, 193);
+            this.txtSlot2.Name = "txtSlot2";
+            this.txtSlot2.Size = new System.Drawing.Size(160, 22);
+            this.txtSlot2.TabIndex = 5;
+            // 
+            // txtSlot3
+            // 
+            this.txtSlot3.Location = new System.Drawing.Point(609, 265);
+            this.txtSlot3.Name = "txtSlot3";
+            this.txtSlot3.Size = new System.Drawing.Size(160, 22);
+            this.txtSlot3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSlot3);
+            this.Controls.Add(this.txtSlot2);
+            this.Controls.Add(this.txtSlot1);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.lv1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader Phone;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.TextBox txtSlot1;
+        private System.Windows.Forms.TextBox txtSlot2;
+        private System.Windows.Forms.TextBox txtSlot3;
+    }
+}
+
---
+++ BaiTap2(28.11)/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BaiTap2_28._11_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            ListViewItem newitem = new ListViewItem(txtSlot1.Text);
+            newitem.SubItems.Add(txtSlot2.Text);
+            newitem.SubItems.Add(txtSlot3.Text);
+            lv1.Items.Add(newitem);
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (lv1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một mục để sửa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ListViewItem selectedItem = lv1.SelectedItems[0];
+
+            selectedItem.Text = txtSlot1.Text;           
+            selectedItem.SubItems[1].Text = txtSlot2.Text;  
+            selectedItem.SubItems[2].Text = txtSlot3.Text; 
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (lv1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một mục để xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn xóa mục này không?",
+                "Xác nhận xóa",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                
+                foreach (ListViewItem item in lv1.SelectedItems)
+                {
+                    lv1.Items.Remove(item); // Xóa mục được chọn
+                }
+            }
+        }
+
+        private void lv1_Click(object sender, EventArgs e)
+        {
+            if (lv1.SelectedIndices.Count > 0)
+            {
+                txtSlot1.Text = lv1.SelectedItems[0].SubItems[0].Text;
+                txtSlot2.Text = lv1.SelectedItems[0].SubItems[1].Text;
+                txtSlot3.Text = lv1.SelectedItems[0].SubItems[2].Text;
+            }
+        }
+    }
+}
